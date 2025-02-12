b3cd46cb5e5cd878565d544030604403ac4f8ec2
palmitex <gabriel1208.palmieri@gmail.com>
2025-02-12 16:27:18 -0300

    fazendo exercicios

---
+++ aula1/ativ1.js
@@ -0,0 +1,36 @@
+// calcular e exibir media entre duas notas
+let nota1 = 10;
+let nota2 = 8;
+
+let media = (nota1 + nota2) / 2;
+
+console.log(`Média das notas: ${media}`);
+
+
+
+//calulcar e exibir IMC
+let altura = 1.77;
+let peso  = 78;
+
+let imc = peso / (altura * altura);
+
+console.log(`Calculo do IMC: ${imc} kg/m2`);
+
+
+
+//calcular e exibir a temperatura de celsius para fahrenheit
+let celsius = 25;
+let fahrenheit = (celsius * 1.8) + 32;
+
+console.log(`Celsius para Fahrenheit: ${fahrenheit} °F`);
+
+
+
+//calcular e exibir preço com desconto
+let preco = 200;
+let desconto = 0.1;
+
+let precoComDesconto = preco * (1 - desconto);
+
+console.log(`Preço com desconto: ${precoComDesconto} R$`);
+
--- aula1/teste.js
+++ aula1/teste.js
@@ -1,3 +1,17 @@
-console.log("Olá, mundo!");
-console.log(`ney melhor do mundo`);
-console.log('aniversario do ney hj');
+// let altura = 1.88;
+// console.log(`Altura: ${altura}`);
+// console.log(typeof altura);
+
+// altura = '1.80'; 
+// console.log(`Altura: ${altura}`); 
+
+{
+    var nome = 'Palmitex';
+    var idade = 30;
+    console.log("Nome: ", nome);
+    console.log(`Idade: ${idade}`);
+
+}
+
+console.log("Nome: ", nome);
+console.log(`Idade: ${idade}`);
